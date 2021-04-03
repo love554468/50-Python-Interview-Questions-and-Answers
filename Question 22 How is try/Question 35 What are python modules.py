e19f78f0462673a432e482587502c49f41db8de2
love554468 <love554468@gmail.com>
2021-04-03 13:30:16 +0700

    update 11-37

---
+++ Question 22 How is try/Question 35 What are python modules.py
@@ -0,0 +1,18 @@
+A python module is a Python file containing a set of function and variables 
+to be used in an application. The variables can be of any type arrays, diction
+, object, etc 
+
+Modules can be either built in or user-defined.
+
+Benefits of modules in Python 
+
+There are a couple of benefits of creating and using a module in Python 
+Structured Code: Logicaaly organized by being grouped into one Python file 
+which makes development easier and less error-prone, code is easier to 
+understand and use 
+
+Reusability : Functionality defined in a single module can be easily 
+reused by other parts of the application..
+
+This eliminates the need to create duplicate code.
+
